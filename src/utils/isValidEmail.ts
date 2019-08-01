c87b747d0c053af2c17616f5524e42d680a72431
Ignas Bernotas <ignas@iber.lt>
2019-08-01 13:43:54 +0300

    Lint fix

--- src/utils/isValidEmail.ts
+++ src/utils/isValidEmail.ts
@@ -1,4 +1,5 @@
 export default (email) => {
+  // eslint-disable-next-line
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return re.test(String(email).toLowerCase())
 }
