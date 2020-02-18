922b1bfcfba2230d78745b15a856bf035d370417
Viktoras <vkuznecovas@users.noreply.github.com>
2020-02-18 10:34:17 +0200

    Fix currency formatting

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,6 +1,6 @@
 const currency = 'MYSTT'
 
 export default function formatCurrency(amount: number) {
-  const val = (amount / 1_000_000).toFixed(3)
+  const val = (amount / 100_000_000).toFixed(3)
   return `${val} ${currency}`
 }
