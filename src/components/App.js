e521990e9e241bb83474a8bc05cef66c393881ae
Varnas <vidmantasvarnas4@gmail.com>
2020-06-17 21:39:26 +0300

    style: changing file names, misstyped first lowercase letter of file

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Route, Redirect} from "react-router-dom";
 import {firsRoute} from  './api/FirstRoute'
 import '../styles/App.scss';
-import Login from './Login/login';
+import Login from './Login/Login';
 import Onboarding from './Onborading/Onboarding';
 
 
