42bd18a48dcbcb4f80f763cdff999cf7832fe722
Varnas <vidmantasvarnas4@gmail.com>
2020-06-21 20:39:15 +0300

    feat: adding .env

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from './components/AppRouter';
+require('dotenv').config();
 
 ReactDOM.render(
   <BrowserRouter>
