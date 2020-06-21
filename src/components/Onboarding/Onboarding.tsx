2342f325963471449f8b12e22bfe1b72c8ac25d6
Varnas <vidmantasvarnas4@gmail.com>
2020-06-21 20:57:51 +0300

    feat: wrapping component and logic to onboarding

--- src/components/Onboarding/Onboarding.tsx
+++ src/components/Onboarding/Onboarding.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import '../../styles/onboarding/main.scss'
+import Welcome from "./steps/Welcome";
+import TermsAndConditions from './steps/TemsAndConditions'
+import {Route, Switch} from "react-router-dom";
+import { Grid } from "@material-ui/core";
 
 const Onboarding = () => {
   return (
-    <div>Onbording</div>
+    <div className="onboarding-wrapper">
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        className="content-box"
+      >
+        <Switch>
+          <Route exact={true}  path={`/onboarding`} component={Welcome}/>
+          <Route exact={true}  path={`/onboarding/terms-and-conditions`} component={TermsAndConditions}/>
+        </Switch>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        className="side-box"
+      >
+      </Grid>
+    </div>
   )
 };
 
---
+++ src/components/Onboarding/steps/TemsAndConditions.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TermsAndConditions = () => {
+  return (
+    <div>Terms and conditions</div>
+  );
+};
+
+export default TermsAndConditions;
