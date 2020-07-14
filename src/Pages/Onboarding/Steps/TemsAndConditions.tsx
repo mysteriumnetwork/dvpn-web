30000fae9d2e25acdabe036138877982881b5b94
Varnas <vidmantasvarnas4@gmail.com>
2020-07-14 20:17:12 +0300

    fix: fixing click handler

--- src/Pages/Onboarding/Steps/TemsAndConditions.tsx
+++ src/Pages/Onboarding/Steps/TemsAndConditions.tsx
@@ -2,23 +2,30 @@ import React from "react";
 import {StepCounter} from "../StepCounter";
 import {acceptWithTermsAndConditions} from "../../../api/User";
 // @ts-ignore
-import { TermsEndUser } from '@mysteriumnetwork/terms'
+import {TermsEndUser} from '@mysteriumnetwork/terms'
 import "../../../assets/styles/pages/onboarding/steps/terms-and-condions.scss"
-import {Link} from "react-router-dom";
+import {withRouter} from "react-router-dom";
 
-  const TermsAndConditions = () => {
-    return (
-      <div className="step-block term-and-conditions">
-        <h1 className="step-block--heading">Terms & Conditions</h1>
-        <div className="step-block-content">
-          <div className="terms-and-conditions">
-            <p>{TermsEndUser}</p>
-          </div>
-          <Link to="/onboarding/service-settings" onClick={acceptWithTermsAndConditions} className="btn btn-filled btn-center accept"><span className="btn-text">I accept</span></Link>
+
+const TermsAndConditions = (props: any) => {
+  const handleAgree = () => {
+    acceptWithTermsAndConditions();
+    props.history.push("/onboarding/service-settings");
+  };
+
+  return (
+    <div className="step-block term-and-conditions">
+      <h1 className="step-block--heading">Terms & Conditions</h1>
+      <div className="step-block-content">
+        <div className="terms-and-conditions">
+          <p>{TermsEndUser}</p>
         </div>
-        <StepCounter step={2}/>
+        <div onClick={handleAgree} className="btn btn-filled btn-center accept"><span
+          className="btn-text">I accept</span></div>
       </div>
-    );
+      <StepCounter step={2}/>
+    </div>
+  );
 };
 
-export default TermsAndConditions;
+export default withRouter(TermsAndConditions);
