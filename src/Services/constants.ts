04c7b459d1da0ab2f663a83fcb5ec0dfcd29caf7
Ignas Bernotas <ignas@iber.lt>
2020-08-10 19:59:17 +0300

    Fix typos in form validation messages

--- src/Services/constants.ts
+++ src/Services/constants.ts
@@ -5,6 +5,6 @@ export const DEFAULT_PRICE_PER_MINUTE_PRICE = 0.005;
 export const DEFAULT_PRICE_PER_GB = 0.005;
 export const DEFAULT_STAKE_AMOUNT = 20;
 
-export const NEW_PASSWORD_ERROR_TO_SHORT = "Password are to short, min 10 characters";
-export const NEW_PASSWORD_ERROR_BLANK = "Passwords fields cannot be blank";
-export const NEW_PASSWORD_ERROR_NOT_SAME = "Password have to be same";
+export const NEW_PASSWORD_ERROR_TO_SHORT = "Your password should be at least 10 characters.";
+export const NEW_PASSWORD_ERROR_BLANK = "Password fields cannot be blank.";
+export const NEW_PASSWORD_ERROR_NOT_SAME = "Passwords have to match.";
