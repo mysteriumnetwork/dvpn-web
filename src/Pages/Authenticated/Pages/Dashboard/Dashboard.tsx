04e683959847728a73c3fd871efa88dd3acf6bd3
Varnas <vidmantasvarnas4@gmail.com>
2020-08-22 17:37:43 +0300

    feat adding earging block

--- src/Pages/Authenticated/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Authenticated/Pages/Dashboard/Dashboard.tsx
@@ -3,18 +3,30 @@ import "../../../../assets/styles/pages/authenticated/pages/dashboard.scss"
 import {ReactComponent as Logo} from '../../../../assets/images/authenticated/pages/dashboard/logo.svg'
 import Header from "../../Components/Header";
 import DashboardTopStatsBlock from "./TopStatBlock";
+import EarningStatisticBlock from './YearningStatiscticBlock'
 
 const Dashboard = () => {
   return (
       <div className="dashboard wrapper">
         <div className="dashboard--content">
           <Header logo={Logo} name="Dashboard"/>
-          <div className="dashboard--top-stats">
+          <div className="dashboard--top-stats-block">
             <DashboardTopStatsBlock stat="35.34 MYST" name="Unsettled earnings"/>
             <DashboardTopStatsBlock stat="4d 2h" name="Sessions time"/>
             <DashboardTopStatsBlock stat="164.3 GB" name="Transferred"/>
             <DashboardTopStatsBlock stat="54" name="Sessions"/>
             <DashboardTopStatsBlock stat="28" name="Unique clients"/>
+          </div>
+          <div className="dashboard--earnings-row">
+              <EarningStatisticBlock
+                month="May"
+                mystValue="48.2223 MYST"
+                earnings="$48.22"
+                residentialPosition="8"
+                residentialPositionOf="250"
+                countryPosition="1"
+                countryPositionOf="8"
+                />
           </div>
         </div>
         <div className="dashboard--side">
---
+++ src/Pages/Authenticated/Pages/Dashboard/YearningStatiscticBlock.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface PropsInterface {
+  month: string
+  mystValue: string,
+  earnings: string,
+  residentialPosition: string,
+  residentialPositionOf: string,
+  countryPosition: string,
+  countryPositionOf: string,
+}
+
+const EarningStatisticBlock: React.FC<PropsInterface> = (_props: PropsInterface) => {
+  const props: PropsInterface = {..._props};
+  return (
+    <div className="dashboard--earnings-block">
+      <p className="heading">Bounty pilot ({props.month})</p>
+      <div className="info-row">
+        <p className="statement">{props.mystValue}</p>
+        <p className="statement-info">{props.earnings}</p>
+      </div>
+      <div className="info-row">
+        <p className="statement">Residential position: {props.residentialPosition} <span>Eligible</span></p>
+        <p className="statement-info">out of {props.residentialPositionOf}</p>
+      </div>
+      <div className="info-row">
+        <p className="statement">Country position: {props.countryPosition} <span>Eligible</span></p>
+        <p className="statement-info">out of {props.countryPositionOf}</p>
+      </div>
+      <div className="more-button">
+        <div className="more-button--dot"></div>
+        <div className="more-button--dot"></div>
+        <div className="more-button--dot"></div>
+      </div>
+    </div>
+  );
+};
+
+export default EarningStatisticBlock;
