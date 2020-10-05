3932d0ca52ffb37fb9ac7589ed4b162a092dbeee
Mantas Domaševičius <mantas@mysterium.network>
2020-10-05 09:58:05 +0300

    Remove un-used dependencies
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -4,10 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { combineReducers } from 'redux';
-
 import sseReducer from '../sse.slice';
 import appReducer from '../app.slice';
+import { combineReducers } from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({
     sse: sseReducer,
