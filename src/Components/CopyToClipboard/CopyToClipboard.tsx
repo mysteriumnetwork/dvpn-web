96649b0e97fd2ed61f611ff074e5a17fa0baca66
Mantas Domaševičius <mantas@mysterium.network>
2021-01-04 11:39:07 +0200

    Use 3rd party copy to clipboard solution to workaround window api
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/Components/CopyToClipboard/CopyToClipboard.tsx
+++ src/Components/CopyToClipboard/CopyToClipboard.tsx
@@ -4,24 +4,27 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react'
-import { copy2clipboard } from '../../commons/clipboard.utils'
+import React, { useEffect } from 'react'
 import FileCopyIcon from '@material-ui/icons/FileCopy'
 import IconButton from '../IconButton/IconButton'
+import Clipboard from 'clipboard'
 
 interface Props {
   text: string
 }
 
 const CopyToClipboard = ({ text }: Props) => {
+  useEffect(() => {
+    const clipboard = new Clipboard('.c2c-btn')
+    return clipboard.destroy
+  })
+
   return (
-    <IconButton
-      onClick={() => {
-        copy2clipboard(text)
-      }}
-    >
-      <FileCopyIcon fontSize="small" />
-    </IconButton>
+    <div className="c2c-btn" data-clipboard-text={text}>
+      <IconButton>
+        <FileCopyIcon fontSize="small" />
+      </IconButton>
+    </div>
   )
 }
 
