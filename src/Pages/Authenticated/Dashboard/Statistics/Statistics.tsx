69db64ef8cce52a5ff13ccce1fbe07af9036fcde
Mantas Domaševičius <mantas@mysterium.network>
2021-09-22 11:20:17 +0300

    Transform earnings statistic card into a hero
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/Pages/Authenticated/Dashboard/Statistics/Statistics.tsx
+++ src/Pages/Authenticated/Dashboard/Statistics/Statistics.tsx
@@ -4,39 +4,52 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { SessionStats } from 'mysterium-vpn-js'
+import { Identity, SessionStats } from 'mysterium-vpn-js'
+import { DEFAULT_MONEY_DISPLAY_OPTIONS } from '../../../../commons'
 
 import { seconds2Time } from '../../../../commons/date.utils'
 import formatBytes, { add } from '../../../../commons/formatBytes'
-import { currentCurrency, displayMyst } from '../../../../commons/money.utils'
+import { displayMyst } from '../../../../commons/money.utils'
+import EarningsStatCard from './EarningsStatCard'
 
-import Statistic from './Statistic'
-import { DEFAULT_MONEY_DISPLAY_OPTIONS } from '../../../../commons'
+import Statistic from './StatCard'
 
 interface Props {
   stats: SessionStats
-  unsettledEarnings: number
+  identity: Identity
   testnet?: boolean
 }
 
-const Statistics = ({ stats, unsettledEarnings, testnet }: Props) => {
+const Statistics = ({ stats, identity, testnet }: Props) => {
   return (
     <>
-      {testnet ? (
-        <Statistic
-          stat={displayMyst(stats.sumTokens, { ...DEFAULT_MONEY_DISPLAY_OPTIONS, showCurrency: false })}
-          name={`Total Earnings (${currentCurrency()})`}
-        />
-      ) : (
-        <Statistic
-          stat={displayMyst(unsettledEarnings, { ...DEFAULT_MONEY_DISPLAY_OPTIONS, showCurrency: false })}
-          name={`Unsettled earnings (${currentCurrency()})`}
-        />
-      )}
       <Statistic stat={seconds2Time(stats.sumDuration)} name="Sessions time" />
       <Statistic stat={formatBytes(add(stats.sumBytesSent, stats.sumBytesReceived))} name="Transferred" />
       <Statistic stat={'' + stats.count} name="Sessions" />
       <Statistic stat={'' + stats.countConsumers} name="Unique clients" />
+      <EarningsStatCard
+        stat={displayMyst(identity.balance, {
+          ...DEFAULT_MONEY_DISPLAY_OPTIONS,
+          fractionDigits: 2,
+        })}
+        name={`Total Earnings`}
+        identity={identity}
+      />
+      {/*{testnet ? (
+        <EarningsStatCard
+          stat={displayMyst(stats.sumTokens, { ...DEFAULT_MONEY_DISPLAY_OPTIONS, showCurrency: false })}
+          name={`Total Earnings (${currentCurrency()})`}
+        />
+      ) : (
+        <EarningsStatCard
+          stat={displayMyst(balance, {
+            ...DEFAULT_MONEY_DISPLAY_OPTIONS,
+            showCurrency: true,
+            fractionDigits: 2,
+          })}
+          name={`Unsettled earnings (${currentCurrency()})`}
+        />
+      )}*/}
     </>
   )
 }
