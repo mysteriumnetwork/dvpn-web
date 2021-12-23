a986ac6a3e211dee8d723ff77e67d1e3669ba332
Mantas Domaševičius <mantas@mysterium.network>
2021-12-23 12:13:58 +0200

    Fix import
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/Pages/Onboarding/steps/TopUpModal/Fiat/cardinity.ts
+++ src/Pages/Onboarding/steps/TopUpModal/Fiat/cardinity.ts
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { PaymentOrder } from '../../../../../../../mysterium-vpn-js'
+import { PaymentOrder } from 'mysterium-vpn-js'
 
 export const CARDINITY_GATEWAY = 'cardinity'
 
