3544ee15a9712677b3a01803e41431ba65d49f17
Mantas Domaševičius <mantas@mysterium.network>
2022-01-14 18:39:05 +0200

    Add stats properly
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/commons/tequil.utils.ts
+++ src/commons/tequil.utils.ts
@@ -9,12 +9,12 @@ import { SessionStats } from 'mysterium-vpn-js'
 const addStats = (a: SessionStats, b: SessionStats): SessionStats => {
   return {
     ...a,
-    count: b.count,
-    countConsumers: b.countConsumers,
-    sumBytesReceived: b.sumBytesReceived,
-    sumBytesSent: b.sumBytesSent,
-    sumDuration: b.sumDuration,
-    sumTokens: b.sumTokens,
+    count: a.count + b.count,
+    countConsumers: a.countConsumers + b.countConsumers,
+    sumBytesReceived: a.sumBytesReceived + b.sumBytesReceived,
+    sumBytesSent: a.sumBytesSent + b.sumBytesSent,
+    sumDuration: a.sumDuration + b.sumDuration,
+    sumTokens: a.sumTokens + b.sumTokens,
   }
 }
 
