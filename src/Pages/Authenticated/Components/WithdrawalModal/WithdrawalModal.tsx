cdf1955c6e4f8814c05860f6391dddb62e9029ea
Mantas Domaševičius <mantas@mysterium.network>
2022-03-03 13:15:29 +0200

    Fix MYST display issue
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/Pages/Authenticated/Components/WithdrawalModal/WithdrawalModal.tsx
+++ src/Pages/Authenticated/Components/WithdrawalModal/WithdrawalModal.tsx
@@ -224,8 +224,8 @@ const WithdrawalModal = ({ isOpen, onClose }: Props) => {
         <InputGroup
           label={`Amount (${currentCurrency()}) / Your Balance: ${myst.display(
             state.balanceTotalWei,
-          )} (Maximum withdrawal
-                  amount: ${MAXIMUM_WITHDRAW_AMOUNT_WEI} ${currentCurrency()})`}
+          )} (Maximum withdrawala
+                  amount: ${myst.display(MAXIMUM_WITHDRAW_AMOUNT_WEI)})`}
         >
           <TextField type="number" value={state.withdrawalAmount.myst} onChange={onWithdrawalAmountChange} />
         </InputGroup>
