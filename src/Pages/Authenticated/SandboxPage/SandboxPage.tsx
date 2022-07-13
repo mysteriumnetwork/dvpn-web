22eda7b54d6cb503b46d549b3b2553ac6770d158
MykolasMaciekus <mykolas@mysterium.network>
2022-07-13 13:09:10 +0300

    Sandbox for settlement status bar

--- src/Pages/Authenticated/SandboxPage/SandboxPage.tsx
+++ src/Pages/Authenticated/SandboxPage/SandboxPage.tsx
@@ -8,8 +8,22 @@ import { Layout, LayoutUnstyledRow } from '../Components/Layout/Layout'
 import { TextField } from '../../../Components/Inputs/TextField'
 import { InputGroup } from '../../../Components/Inputs/InputGroup'
 import { InputLockIcon } from '../../../Components/Icons/InputIcons'
+import { ProgressBar } from '../../../Components/ProgressBar/ProgressBar'
+import { Slider } from '../../../Components/Slider/Slider'
+import { useState } from 'react'
+import styled from 'styled-components'
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 30px;
+`
 export const SandboxPage = () => {
+  const [earnings, setEarnings] = useState(0)
+
   return (
     <Layout>
       <LayoutUnstyledRow>
@@ -35,6 +49,22 @@ export const SandboxPage = () => {
           subTitle="Mb"
           input={<TextField error value="test" icon={<InputLockIcon $variant="error" />} />}
         />
+      </LayoutUnstyledRow>
+      <LayoutUnstyledRow>
+        <Container>
+          <ProgressBar size={'small'} settleThresholdMyst={5} earningsTokens={earnings} />
+          <ProgressBar size={'big'} settleThresholdMyst={5} earningsTokens={earnings} />
+          <Slider
+            step={0.01}
+            min={0}
+            max={5}
+            value={earnings}
+            onChange={(earnings) => {
+              /* @ts-ignore */
+              setEarnings(earnings)
+            }}
+          />
+        </Container>
       </LayoutUnstyledRow>
     </Layout>
   )
