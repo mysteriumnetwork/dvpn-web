aafb4557d528262f841f10998f3925e2a9fdbbd4
MykolasMaciekus <mykolas@mysterium.network>
2022-07-18 09:41:03 +0300

    Pagination component logic

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -7,6 +7,7 @@
 import styled from 'styled-components'
 import { Button } from '../Inputs/Button'
 import { themeCommon } from '../../theme/themeCommon'
+import { useState } from 'react'
 
 const Container = styled.div`
   width: 100%;
@@ -14,15 +15,20 @@ const Container = styled.div`
   justify-content: space-between;
   padding: 20px 10px;
 `
-const PageButton = styled.button`
+const PageButton = styled.button<Props>`
   height: 32px;
   width: 32px;
   border-radius: 50%;
-  color: ${themeCommon.colorGrayBlue};
+  color: ${({ theme }) => theme.common.colorGrayBlue};
   background: none;
-  border: 1px solid ${themeCommon.colorGrayBlue};
-
+  border: 1px solid ${({ theme }) => theme.common.colorGrayBlue};
+  :hover {
+    cursor: pointer;
+  }
   :active {
+    background: ${themeCommon.colorGrayBlue}51;
+  }
+  :disabled {
     background: ${themeCommon.colorGrayBlue}51;
   }
 `
@@ -30,16 +36,33 @@ const Pages = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 5px;
+  gap: 10px;
 `
+interface Props {
+  onClick?: () => void
+}
+// TODO: Hook up to API, rinse logic for displaying active page
+
 export const Pagination = () => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [pages, setPages]: Array<any> = useState([1, 2, 3, 4, 5])
+
   return (
     <Container>
       <Button variant="outlined" label="Prev" rounded />
       <Pages>
-        <PageButton>1</PageButton>
-        <PageButton>2</PageButton>
-        <PageButton>3</PageButton>
+        {pages.map((_: any, i: number) => (
+          <PageButton
+            disabled={i + 1 === currentPage}
+            key={i}
+            onClick={() => {
+              setCurrentPage(i + 1)
+              console.log(currentPage)
+            }}
+          >
+            {i + 1}
+          </PageButton>
+        ))}
       </Pages>
       <Button variant="outlined" label="Next" rounded />
     </Container>
