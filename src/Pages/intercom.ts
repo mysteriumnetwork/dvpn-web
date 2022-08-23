bb226f278244585b433bceb8ef0db84dcfc18c70
Mantas Domaševičius <mantas@mysterium.network>
2022-08-23 07:16:49 +0300

    Rename
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/Pages/intercom.ts
+++ src/Pages/intercom.ts
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { localStorageKeys } from '../constants/local_storage_keys'
+import { localStorageKeys } from '../constants/local-storage.keys'
 
 export const loadIntercomCookie = () => {
   const intercomUserId = localStorage.getItem(localStorageKeys.INTERCOM_USER_ID)
