803f4fe25b2e6851cb4c685a1e36e0d635357833
Mantas Domaševičius <mantas@mysterium.network>
2022-08-31 19:06:48 +0300

    Throw away un-used code
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/commons/myst.test.ts
+++ src/commons/myst.test.ts
@@ -24,28 +24,10 @@ test('options', () => {
 })
 
 test.each([
-  { wei: '6123456789123456789', expected: '6.123456789123456789' },
-  { wei: '23456789123456789', expected: '0.023456789123456789' },
-  { wei: '1', expected: '0.000000000000000001' },
-  { wei: '0', expected: '0' },
-])('toEtherString: %s', (data) => {
-  expect(myst.toEtherString(data.wei)).toEqual(data.expected)
-})
-
-test.each([
   { wei: '6123456789123456789', expected: '6.1234567' },
   { wei: '23456789123456789', expected: '0.0234567' },
   { wei: '1', expected: '0.0000000' },
   { wei: '0', expected: '0.0000000' },
 ])('toEtherBig: %s', (data) => {
   expect(myst.toEtherBig(data.wei).toFixed(7)).toEqual(data.expected)
-})
-
-test.each([
-  { wei: '6.123456789123456789', expected: '6123456789123456789' },
-  { wei: '0.023456789123456789', expected: '23456789123456789' },
-  { wei: '0.000000000000000001', expected: '1' },
-  { wei: '0', expected: '0' },
-])('toWeiString: %s', (data) => {
-  expect(myst.toWeiString(data.wei)).toEqual(data.expected)
 })
