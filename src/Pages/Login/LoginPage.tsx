f95a71a50fec126032ae392dd9dad7be0ce88ada
MykolasMaciekus <mykolas@mysterium.network>
2022-09-07 17:37:55 +0300

    Welcome page

--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -10,7 +10,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { tequila } from '../../api/tequila'
 import { DEFAULT_USERNAME } from '../../constants/defaults'
 import styled from 'styled-components'
