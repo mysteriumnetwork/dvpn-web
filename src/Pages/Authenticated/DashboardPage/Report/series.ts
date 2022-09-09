fd6ae47bc91a11d5dcb9b7a25d0a9f1453ec8122
Mantas Domaševičius <mantas@mysterium.network>
2022-09-09 15:18:15 +0300

    Correct import
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/Pages/Authenticated/DashboardPage/Report/series.ts
+++ src/Pages/Authenticated/DashboardPage/Report/series.ts
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { SeriesResponse } from 'mysterium-vpn-js/lib/provider'
+import { SeriesResponse } from 'mysterium-vpn-js'
 import { ChartType, Pair } from './types'
 import { currentCurrency } from '../../../../commons/currency'
 import { MetricsRange } from '../../../../types/common'
