64c5df1a16b2669ebe493beff9bae65d29b35768
Mantas Domaševičius <mantas@mysterium.network>
2022-09-28 09:50:06 +0300

    Simplify node update notifications
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/Pages/Authenticated/Components/Notifications/NodeReleaseCheck.tsx
+++ src/Pages/Authenticated/Components/Notifications/NodeReleaseCheck.tsx
@@ -5,20 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 import { useAppSelector } from '../../../../commons/hooks'
-import remoteStorage from '../../../../commons/remoteStorage'
-import { useEffect, useMemo } from 'react'
+import { useEffect } from 'react'
 import { selectors } from '../../../../redux/selectors'
 import { fetchLatestNodeVersion } from '../../../../api/node-version.management'
-import errors from '../../../../commons/errors'
-
-const ONE_HOUR_MS = 2 * 60 * 60 * 1000
-export const KEY_NODE_VERSION_CHECK = `NODE_VERSION_CHECK`
-
-interface NodeVersionCheck {
-  latestVersion: string
-  checkedAt: number
-  versionAtCheck: string
-}
+import semver from 'semver/preload'
 
 interface Props {
   onNewNodeVersionAvailable: (currentVersion: string, latestVersion: string) => void
@@ -26,38 +16,18 @@ interface Props {
 
 export const NodeReleaseCheck = ({ onNewNodeVersionAvailable }: Props) => {
   const healthCheck = useAppSelector(selectors.healthCheck)
-  const check = useAppSelector(remoteStorage.selector<NodeVersionCheck>(KEY_NODE_VERSION_CHECK))
 
   useEffect(() => {
     ;(async () => {
-      if (check?.latestVersion && healthCheck.version.toString() !== check.latestVersion) {
-        onNewNodeVersionAvailable(healthCheck.version, check.latestVersion)
+      const latest = await fetchLatestNodeVersion()
+      const current = healthCheck.version
+
+      if (!semver.valid(latest) || !semver.valid(current)) {
+        return
       }
-    })()
-  }, [check?.latestVersion, healthCheck.version])
 
-  const now = useMemo(() => {
-    return Date.now()
-  }, [])
-
-  const fetchLatestVersionOrDefault = async () => {
-    if (!check || now > check.checkedAt + ONE_HOUR_MS || check.versionAtCheck !== healthCheck.version) {
-      return await fetchLatestNodeVersion()
-    }
-
-    return check.latestVersion
-  }
-
-  useEffect(() => {
-    ;(async () => {
-      try {
-        await remoteStorage.put<NodeVersionCheck>(KEY_NODE_VERSION_CHECK, {
-          latestVersion: await fetchLatestVersionOrDefault(),
-          checkedAt: now,
-          versionAtCheck: healthCheck.version,
-        })
-      } catch (err: any) {
-        errors.parseToastError(err)
+      if (semver.gt(latest, current)) {
+        onNewNodeVersionAvailable(current, latest)
       }
     })()
   }, [])
