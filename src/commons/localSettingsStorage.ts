2759f9081de7e0c92100e2bc2ccb81f559b050f4
Mantas Domaševičius <mantas@mysterium.network>
2023-05-29 11:29:40 +0300

    Comment
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/commons/localSettingsStorage.ts
+++ src/commons/localSettingsStorage.ts
@@ -8,24 +8,30 @@ import { RootState, store } from '../redux/store'
 import { updateNodeUISettingsStore } from '../redux/app.slice'
 import { localStorageKeys } from '../constants/local-storage.keys'
 import localStorage from './localStorageWrapper'
+
 const { SETTINGS } = localStorageKeys
+
 const root = (): { [key: string]: any } => {
   return store.getState().app.nodeUISettings || {}
 }
+
 const get = (key: string): { [key: string]: any } => {
   const settings = localStorage.get(SETTINGS) as any
   return (settings || {})[key]
 }
+
 const put = <T extends any>(key: string, value: T) => {
   const storageRoot = root()
   localStorage.put(SETTINGS, { ...storageRoot, [key]: value })
   store.dispatch(updateNodeUISettingsStore({ ...storageRoot, [key]: value }))
 }
+
 const selector = <T>(key: string): ((state: RootState) => T | undefined) => {
   return ({ app: { nodeUISettings } }: RootState) => {
     return (nodeUISettings || {})[key]
   }
 }
+
 const localSettingsStorage = {
   get,
   put,
