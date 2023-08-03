02b900853c3e7734a7828f96918f9c9c4cfb470c
Mantas Domaševičius <mantas@mysterium.network>
2023-08-03 10:46:50 +0300

    Fix doc links
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -5,11 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export const DOCS = 'https://docs.mysterium.network'
-export const DOCS_NAT_FIX = `${DOCS}/troubleshooting/nat-traversal`
-export const DOCS_FORGOT_PASSWORD = `${DOCS}/troubleshooting/forgot-password`
-export const DOCS_METAMASK = `${DOCS}/for-node-runners/how-to-setup-polygon-myst-on-metamask`
-export const PAYOUT_GUIDE = `${DOCS}/for-node-runners/payout-guide`
-export const DOCS_UPDATE_NODE = `${DOCS}/for-node-runners/updating-your-node`
-
+export const DOCS = 'https://help.mystnodes.com'
+export const DOCS_NAT_FIX = `${DOCS}/en/articles/8205152-node-nat-statuses`
+export const DOCS_FORGOT_PASSWORD = `${DOCS}/en/articles/8005248-forgot-password`
+export const DOCS_METAMASK = `${DOCS}/en/articles/8004186-adding-myst-token-to-metamask-on-the-polygon-mainnet`
+export const PAYOUT_GUIDE = `${DOCS}/en/articles/8004190-where-to-buy-polygon-myst`
+export const DOCS_UPDATE_NODE = `${DOCS}/en/articles/8005521-updating-your-node`
 export const ANDROID_DEEPLINK = 'mystnodes://sso'
