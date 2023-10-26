66ce801f7ecbfb227ec4055405bf663cfd4cde5f
mingaila <mind4surf@gmail.com>
2023-10-26 13:29:25 +0200

    Cleaned up unused code

--- src/intercom/intercom.tsx
+++ src/intercom/intercom.tsx
@@ -4,11 +4,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { useEffect } from 'react'
 import { SupportIssueResponse } from 'mysterium-vpn-js'
-import { localStorageKeys } from '../constants/local-storage.keys'
 import { useAppSelector } from '../commons/hooks'
-import complexActions from '../redux/complex.actions'
 
 const DESKTOP_OPTIONS = {
   hide_default_launcher: true,
@@ -25,25 +22,13 @@ const MOBILE_OPTIONS = {
 }
 
 export const initIntercom = () => {
-  const intercomUserId = localStorage.getItem(localStorageKeys.INTERCOM_USER_ID)
-  if (intercomUserId !== null && intercomUserId !== '') {
-    // @ts-ignore
-    window.Intercom('boot', {
-      anonymous_id: intercomUserId,
-      ...DESKTOP_OPTIONS,
-    })
-  } else {
-    // Load intercom chat
-    // @ts-ignore
-    window.Intercom('boot', {
-      app_id: 'h7hlm9on',
-      ...DESKTOP_OPTIONS,
-    })
-  }
+  window.Intercom('boot', {
+    app_id: 'h7hlm9on',
+    ...DESKTOP_OPTIONS,
+  })
 }
 
 export const updateIntercom = (mode: 'mobile' | 'desktop') => {
-  // @ts-ignore
   window.Intercom('update', {
     ...(mode === 'mobile' ? MOBILE_OPTIONS : DESKTOP_OPTIONS),
   })
@@ -52,22 +37,11 @@ export const updateIntercom = (mode: 'mobile' | 'desktop') => {
 export const useIntercom = () => {
   const open = useAppSelector(({ app }) => app.chatOpened)
 
-  useEffect(() => {
-    // @ts-ignore
-    window.Intercom('onHide', () => complexActions.setChatOpened(false))
-    // @ts-ignore
-    window.Intercom('onShow', () => complexActions.setChatOpened(true))
-  }, [])
-
-  // @ts-ignore
   const show = () => window.Intercom('show')
-  // @ts-ignore
   const showNewMessage = (message: string) => window.Intercom('showNewMessage', message)
-  // @ts-ignore
   const hide = () => window.Intercom('hide')
 
   const reportIssue = ({ message, email, identity, nodeCountry, ipType, ip }: SupportIssueResponse) => {
-    // @ts-ignore
     window.Intercom('update', {
       anonymous_email: email,
       node_country: nodeCountry,
