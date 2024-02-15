9d982a49fb1a5e734d6f131ca85345d29c829128
Mantas Domaševičius <mantas@mysterium.network>
2024-02-15 13:24:33 +0200

    Tailwind spinner
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#d61f85",
+        primaryLight: "#ed5bac",
+      },
+    },
   },
   plugins: [],
 };
